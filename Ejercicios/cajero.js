3a293d5e57ce6833c5d378d33e82f6096d46ac5a
SaulSavi <1575saavedra@gmail.com>
2020-05-01 18:58:24 -0500

    Avance Ejercicios

---
+++ Ejercicios/cajero.js
@@ -0,0 +1,7 @@
+//Ejercicio cajero (12)
+// Crear una clase Cuenta que tenga los siguientes atributos y metodos:
+// -Titular y cantidad
+// -ingresar(cantidad)
+// -retirar(cantidad)
+// Hacer las validaciones previas
+// Como regla de negocio no debe de tener más de $900 y menos de $10
--- Ejercicios/rfc.js
+++ Ejercicios/rfc.js
@@ -1,3 +1,11 @@
+//Ejercicio RFC (11)
+// Haz una clase llamada Persona que siga las siguientes condiciones:
+// 	 Sus atributos son: primer nombre,segundo nombre,Primer Apellido, Segundo Apellido ,fecha nacimiento, edad,RFC, sexo, peso y altura.
+// 	 calcularIMC()
+// 	 esMayorDeEdad()
+// 	 El constructor pide  primer nombre,segundo nombre,Primer Apellido, Segundo Apellido ,fecha nacimiento, edad, sexo, peso y altura.
+// 	 Generar el RFC a partir de el nombre, edad y sexo
+
 class Persona {
 
     constructor(primer_nombre,segundo_nombre,primer_apellido,segundo_apellido,fecha_nacimiento,edad,genero,peso,altura){
@@ -13,17 +21,19 @@ class Persona {
     }
 
     calcularIMC(){
-        return "IMC: "+this.peso/ this.altura**2
+        console.log("IMC: "+this.peso/ this.altura**2)
     }
 
     esMayorDeEdad(){
         if(this.edad>=18){
-            return "Mayor de edad"
+             console.log("Mayor de edad")
         }else{
-            return "Menor de edad"
+            console.log("Menor de edad")
         }
-    }
-
+    }  
     
 }
 
+let Carlos = new Persona('Carlos','Adrian','Sanchez','Hernandez','14/03/1983',37,'M',83,1.75)
+
+console.log(Carlos);
