c344652406928b3042d6863393f94d574f73c629
SaulSaVi <1575saavedra@gmail.com>
2020-05-04 23:19:52 -0500

    incio requests

---
+++ peticiones/index.js
@@ -0,0 +1,22 @@
+const request = require('request');
+
+function atrapar(error,response,body){//Este es mi callback
+    console.log(response.statusCode) //200
+    const data = JSON.parse(body)//convierte un JSON en js
+    
+    // for(let i=0; i < data.results.length; i++){
+    //     console.log(data.results[i].name)
+    // }
+    data.results.map((person) => {
+        console.log(person.name)
+    })
+    console.log("===== Segundo For =====")
+    for (const person of data.results){
+        console.log(person.name)
+    }
+
+}
+
+
+
+request('https://swapi.dev/api/people/', atrapar)
