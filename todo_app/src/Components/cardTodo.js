c2db790af1f0c5b989de1e2ecbae069785e1cabc
SaulSaVi <1575saavedra@gmail.com>
2020-05-15 15:59:53 -0500

    intro firebase

---
+++ todo_app/src/Components/cardTodo.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+function CardTodo(props){
+
+    const bg ={
+        "1":"card text-white bg-success my-3",
+        "2":"card text-white bg-warning my-3",
+        "3":"card text-white bg-danger my-3"
+    }
+
+    return(
+        <div className={bg[props.prioridad]}>
+            <div className="card-header">  Creado por:{props.nombre}</div>
+            <div className="card-body">
+                <p className="card-text">{props.todo}</p>
+            </div>
+        </div>
+    )
+}
+
+export default CardTodo
---
+++ todo_app/src/Components/home.js
@@ -0,0 +1,94 @@
+import React, { useState, useEffect  } from 'react';
+import axios from 'axios';
+import CardTodo from './CardTodo';
+import Navbar from './Navbar';
+
+
+function Home(){
+    const [todosAlta,setTodosAlta] = useState([])
+    const [todosMedia,setTodosMedia] = useState([])
+    const [todosBaja,setTodosBaja] = useState([])
+
+
+    useEffect(() => {
+        axios.get('https://todoapp-e1226.firebaseio.com/todos.json')
+            .then((response) => {
+                console.log(response.data) //Esto me trajo firebase
+                const elements = Object.entries(response.data).reverse() //esto combierte objetos en arreglos
+                console.log(elements)
+                const realData = elements.map((todo) =>{ //Convertir un arreglo de arreglos a un arreglo de objetos
+                    const [id,data] = todo; //Estoy destructurando el arreglo de adentro
+                    return {
+                        id,
+                        ...data //split object hago una copia de la data
+                    }
+                } )
+                console.log(realData)
+                const alta = realData.filter((todo) => todo.prioridad === "3") //filtra todos por prioridad 3
+                const media = realData.filter((todo) => todo.prioridad === "2" ) //filtra todos por prioridad 2
+                const baja = realData.filter((todo) => todo.prioridad === "1" ) //filtra todos por prioridad 1
+                setTodosAlta(alta)
+                setTodosMedia(media)
+                setTodosBaja(baja)
+            }).catch((error) => {
+                alert(error)
+            })
+    },[])
+
+    return(
+        <div className="App">
+            <Navbar/>
+            <div className="container mt-5 pt-5">
+                <div className="row justify-content-center mb-5">
+                    <div className="col-4">
+                        <a href="/createTodo" className="btn btn-lg bg-dark text-white">
+                          Crear to-do
+                        </a>
+                    </div>
+                </div>
+                <div className="row justify-content-center">   
+                    <div className="col-12 col-sm-4 col-md-4 col-lg-4">
+                        <h4>Importantes</h4>
+                        { todosAlta.map((todo) => {
+                            return(
+                                <CardTodo 
+                                    nombre={todo.user}  
+                                    todo={todo.todo}
+                                    prioridad={todo.prioridad}
+                                />
+                            )
+                        }) }
+                    </div>
+                    <div className="col-12 col-sm-4 col-md-4 col-lg-4">
+                    <h4>No pasa nada</h4>
+                    { todosMedia.map((todo) => {
+                            return(
+                                <CardTodo 
+                                    nombre={todo.user}  
+                                    todo={todo.todo}
+                                    prioridad={todo.prioridad}
+                                />
+                            )
+                        }) }
+                    </div>
+                    <div className="col-12 col-sm-4 col-md-4 col-lg-4">
+                    <h4>Pueden esperar</h4>
+                    { todosBaja.map((todo) => {
+                            return(
+                                <CardTodo 
+                                    nombre={todo.user}  
+                                    todo={todo.todo}
+                                    prioridad={todo.prioridad}
+                                />
+                            )
+                        }) }
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    )
+
+}
+
+export default Home;
